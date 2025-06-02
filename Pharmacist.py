b836e9856cc2a361283d3cbad9d84e9bd5a9fe2c
MonkeyDSamuel <samueleternityunofficial@gmail.com>
2025-06-02 14:28:09 +0530

    TRY EXCEPTIONS ADDED

--- Pharmacist.py
+++ Pharmacist.py
@@ -20,7 +20,10 @@ def Login():
     print("-------WELCOME DEAR PHARMACIST-------")
     u_name = input("Enter the username of the pharmacist: ")
     pwd = input("Enter the password of the pharmacist: ")
-    if(u_name == username and pwd == password):
+    if(u_name=="" or pwd==""):
+        print("-------PLEASE ENTER YOUR LOGIN CREDENTIALS-------")
+        return False
+    elif(u_name == username and pwd == password and u_name!="" and pwd!=""):
         print("-------LOGIN SUCCESSFUL-------")
         return True
     else:
@@ -30,50 +33,59 @@ def Login():
 # FUNCTION TO ADD MEDICINE
 def Add_med():
     print("-------MEDICINE DETAILS-------")
-    med_ID = int(input("Enter the ID of the medicine:"))
-    if med_ID in medicines:
-        print("MEDICINE ALREADY EXISTS IN THE PHARMACY")
-    else:
-        name = input("Enter the name of the medicine: ")
-        company = input("Enter the company name of the medicine: ")
-        generic_name = input("Enter the generic name of the medicine: ")
-        price = input("Enter the price of the medicine: ")
-        medicines[med_ID] = {"name":name, "company":company, "generic_name":generic_name, "price":price}
-
+    try:
+        med_ID = int(input("Enter the ID of the medicine:"))
+        if med_ID in medicines:
+            print("MEDICINE ALREADY EXISTS IN THE PHARMACY")
+        else:
+            name = input("Enter the name of the medicine: ")
+            company = input("Enter the company name of the medicine: ")
+            generic_name = input("Enter the generic name of the medicine: ")
+            price = int(input("Enter the price of the medicine: "))
+            medicines[med_ID] = {"name":name, "company":company, "generic_name":generic_name, "price":price}
+    except:
+        print("PLEASE ENTER THE APPROPRIATE VALUES")
 # FUNCTION TO DELETE MEDICINE FROM THE LIST
 def Del_med():
     print("-------MEDICINE DELETION FROM PHARMACY-------")
-    med_ID = int(input("Enter the ID of the medicine: "))
-    if med_ID in medicines:
-        del medicines[med_ID]
-    else:
-        print("-------INVALID MEDICINE ID-------THE MEDICINE DOES NOT EXIST-------")
-
+    try:
+        med_ID = int(input("Enter the ID of the medicine:"))
+        if med_ID in medicines:
+            del medicines[med_ID]
+        else:
+            print("-------INVALID MEDICINE ID-------THE MEDICINE DOES NOT EXIST-------")
+    except:
+        print("PLEASE INPUT INTEGER VALUES FOR MED ID")
 # FUNCTION TO UPDATE MEDICINE DETAILS
 def Upd_med():
     print("-------MEDICINE DETAILS UPDATION-------")
-    med_ID = int(input("Enter the ID of the medicine: "))
-    if med_ID in medicines:
-        name = input(f"NEW MEDICINE NAME[{medicines[med_ID]["name"]}]: " or medicines[med_ID]["name"])
-        company = input(f"NEW COMPANY NAME[{medicines[med_ID]["company"]}]: " or medicines[med_ID]["company"])
-        generic_name = input(f"UPDATED GENERIC NAME[{medicines[med_ID]["generic_name"]}]: " or medicines[med_ID]["generic_name"])
-        price = input(f"NEW UPDATED PRICE[{medicines[med_ID]["price"]}]: " or medicines[med_ID]["price"])
-        medicines[med_ID] = {"name":name, "company":company, "generic_name":generic_name, "price":price}
-    else:
-        print("-------MEDICINE ID NOT FOUND-------")
-
+    try:
+        med_ID = int(input("Enter the ID of the medicine:"))
+        if med_ID in medicines:
+            name = input(f"NEW MEDICINE NAME[{medicines[med_ID]["name"]}]: " or medicines[med_ID]["name"])
+            company = input(f"NEW COMPANY NAME[{medicines[med_ID]["company"]}]: " or medicines[med_ID]["company"])
+            generic_name = input(f"UPDATED GENERIC NAME[{medicines[med_ID]["generic_name"]}]: " or medicines[med_ID]["generic_name"])
+            price = int(input(f"NEW UPDATED PRICE[{medicines[med_ID]["price"]}]: " or medicines[med_ID]["price"]))
+            medicines[med_ID] = {"name":name, "company":company, "generic_name":generic_name, "price":price}
+        else:
+            print("-------MEDICINE ID NOT FOUND-------")
+    except:
+        print("PLEASE INPUT THE APPROPRIATE VALUES")
 # FUNCTION TO SEARCH FOR MEDICINE USING ID
 def Search_med():
     print("-------MEDICINE SEARCH FROM PHARMACY-------")
-    med_ID = int(input("Enter the ID of the medicine:"))
-    if med_ID in medicines:
-        print("-------MEDICINE FOUND-------")
-        print(f"MEDICINE NAME: {medicines[med_ID]["name"]}")
-        print(f"COMPANY NAME: {medicines[med_ID]["company"]}")
-        print(f"GENERIC NAME: {medicines[med_ID]["generic_name"]}")
-        print(f"PRICE: {medicines[med_ID]["price"]}")
-    else:
-        print("-------MEDICINE NOT FOUND-------")
+    try:
+        med_ID = int(input("Enter the ID of the medicine:"))
+        if med_ID in medicines:
+            print("-------MEDICINE FOUND-------")
+            print(f"MEDICINE NAME: {medicines[med_ID]["name"]}")
+            print(f"COMPANY NAME: {medicines[med_ID]["company"]}")
+            print(f"GENERIC NAME: {medicines[med_ID]["generic_name"]}")
+            print(f"PRICE: {medicines[med_ID]["price"]}")
+        else:
+            print("-------MEDICINE NOT FOUND-------")
+    except:
+        print("PLEASE THE APPROPRIATE VALUES")
 
 # FUNCTION TO VIEW ALL MEDICINES FROM THE LIST
 def View_med():
